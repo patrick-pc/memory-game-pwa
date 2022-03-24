aecdc371190c586c6faf9cccbaa0f750fed5cca0
Peter Parker <web3slinger@gmail.com>
2022-03-24 10:53:14 +0800

    Add game logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
-const Card = ({ image, selected, onCLick }) => {
+const Card = ({ image, selected, onClick }) => {
   return (
     <div className="card">
-      <div className={selected ? 'selected' : undefined}>
+      <div className={selected && 'selected'}>
         <img alt="" src={image} className="card-face" />
 
-        <img alt="" src={'/assets/fireship.png'} className="card-back" />
+        <img
+          alt=""
+          className="card-back"
+          src={'/assets/fireship.png'}
+          onClick={onClick}
+        />
       </div>
     </div>
   )
